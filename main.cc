f9daf022636135a614038da5ee69697200fcead0
Ji Hwan An <angeljihwan@gmail.com>
2025-02-10 13:13:47 +0900

    Single ball

--- main.cc
+++ main.cc
@@ -12,18 +12,46 @@ using namespace std;
 #define VIEW_WIDTH (1.5f * WINDOW_WIDTH)
 #define VIEW_HEIGHT (1.5f * WINDOW_HEIGHT)
 
-void Render(void) {
+#define RADIUS 80.f
+
+// x: position
+struct Particle {
+  Particle(float _x, float _y) : x(_x, _y) {}
+  Vector2d x;
+};
+
+static vector<Particle> particles;
+
+void aqua_render(void) {
   glClear(GL_COLOR_BUFFER_BIT);
 
   glLoadIdentity();
   glOrtho(0, VIEW_WIDTH, 0, VIEW_HEIGHT, 0, 1);
 
+  glColor4f(0.2f, 0.6f, 1.f, 1);
+  glBegin(GL_POINTS);
+  for (auto &p : particles) {
+    glVertex2f(p.x(0), p.x(1));
+  }
+  glEnd();
+
   glutSwapBuffers();
 }
 
-void InitGL(void) {
+void aqua_update(void) {
+  (void)0;
+  glutPostRedisplay();
+}
+
+void aqua_init(void) {
+  (void)0;
+  particles.push_back(Particle(VIEW_WIDTH / 2, VIEW_HEIGHT / 2));
+}
+
+void aqua_gl_init(void) {
   glClearColor(0.9f, 0.9f, 0.9f, 1.f);
   glEnable(GL_POINT_SMOOTH);
+  glPointSize(RADIUS);
   glMatrixMode(GL_PROJECTION);
 }
 
@@ -32,9 +60,11 @@ int main(int argc, char **argv) {
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
   glutInit(&argc, argv);
   glutCreateWindow("Aqua");
-  glutDisplayFunc(Render);
+  glutDisplayFunc(aqua_render);
+  glutIdleFunc(aqua_update);
 
-  InitGL();
+  aqua_init();
+  aqua_gl_init();
 
   glutMainLoop();
 }
